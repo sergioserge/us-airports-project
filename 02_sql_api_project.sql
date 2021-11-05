61a37feb478e3e2f84a98fded1eae26a6bb6cab4
Parahn <paulrahn01@gmail.com>
2021-11-05 18:19:38 +0100

    Update notebooks

--- 02_sql_api_project.sql
+++ 02_sql_api_project.sql
@@ -1,62 +1,78 @@
--- csv file from the website https://transtats.bts.gov
--- Hypothesis
--- Cancellation and delays appeared parallel to the hurricane path.
--- There is a threshold (wind, rain, fog) until the flights are not cancelled.
--- Many delays up to two weeks after the hurricane.
+/*	This is a csv file from the website https://transtats.bts.gov
+	The timeline is from 2012-10-01 to 2012-11-30
+	It contains flights data from six airports
+	- ['BOS', 'EWR', 'JFK', 'MIA', 'PHI', 'SJU']
+	- Boston (BOS)
+	- Miami (MIA)
+	- Newark (EWR)
+	- New York (JFK)
+	- Philadelphia (PHL)
+	- Purto Rico (SJU)
+*/
 
--- Which columns/data do we need do we need?
-
-SELECT flight_date,
-	   dep_time,
-	   dep_delay,
-	   arr_delay,
-	   airline, -- Difference between airlines
-	   tail_number,
-	   origin,
-	   dest,
-	   diverted,
-	   cancelled
-FROM flights_api_proj_gr4;
-
-
-
-
-
-
-
-
-
--- Show table
+-- Show all data
 SELECT * FROM flights_api_proj_gr4  AS f
 
 
--- EDA in DBeaver
-SELECT COUNT(*) FROM flights_api_proj_gr4;
-SELECT MAX(flight_date), MIN(flight_date) FROM flights_api_proj_gr4;
-SELECT COUNT(*) FROM flights_api_proj_gr4
-WHERE cancelled = 1;
-
---
-SELECT origin FROM flights_api_proj_gr4 
-GROUP BY origin
-HAVING origin IN['BOS', 'JFK', 'MIA', 'EWR', 'PHL'];
-
--- standard
-SELECT a.tz AS origin_tz,
-	   a.tz AS dest_tz,
+-- get the latitude, longitude or city names for the airpots in the list
+SELECT a.faa,
 	   a.name AS name,
-	   f.origin,
-	   f.dest,
-	   f.dep_time,
-	   f.arr_time,
-	   f.air_time,
-	   MAKE_TIME(FLOOR(f.dep_time/100)::int, (f.dep_time::int%100), 0) AS dep_time_f,
-	   MAKE_TIME(FLOOR(f.arr_time/100)::int, (f.arr_time::int%100), 0) AS arr_time_f,
-	   MAKE_TIME(FLOOR(f.arr_time/100)::int, (f.arr_time::int%100), 0) - MAKE_TIME(FLOOR(f.dep_time/100)::int, (f.dep_time::int%100), 0) AS calc_air_time,
-	   MAKE_INTERVAL(mins => (f.air_time::int)) AS air_time_f
+	   a.lat,
+	   a.lon,
+	   a.alt
 FROM flights_api_proj_gr4 f
 LEFT JOIN airports a
-	ON f.origin = a.faa;
+	ON f.origin = a.faa
+GROUP BY a.faa, a.name, a.lat, a.lon, a.alt
+HAVING faa IN ( 'BOS', 'EWR', 'JFK', 'MIA', 'PHI', 'SJU' )
+ORDER BY faa;
+
+
+-- time period and amount of data
+SELECT MIN(flight_date), MAX(flight_date), COUNT(*) FROM flights_api_proj_gr4;
+
+
+-- aggregated data: flights cancelled
+SELECT flight_date, count(*) 
+FROM flights_api_proj_gr4 f
+GROUP BY flight_date, cancelled 
+HAVING cancelled = 1
+ORDER BY count(*) DESC
+LIMIT 8;
+
+-- Aggregated data: delay
+SELECT AVG(dep_delay) AS dep_delay_avg,
+	   MIN(dep_delay) AS dep_delay_min,
+	   MAX(dep_delay) AS dep_delay_max,
+	   AVG(arr_delay) AS arr_delay_avg,
+	   MIN(arr_delay) AS arr_delay_min,
+	   MAX(arr_delay) AS arr_delay_max,
+	   (SELECT COUNT(*) FROM flights_api_proj_gr4 WHERE diverted = 1) AS diverted,
+	   (SELECT COUNT(*) FROM flights_api_proj_gr4 WHERE cancelled = 1) AS cancelled
+FROM flights_api_proj_gr4 f;
+
+
+-- Which columns/data do we need?
+
+SELECT flight_date,
+	   origin,
+	   dest,
+	   -- dep_time,
+	   MAKE_TIME(FLOOR(f.dep_time/100)::int, (f.dep_time::int%100), 0) AS dep_time_f, -- CONVERT DATA TYPE
+	   -- arr_time,
+	   MAKE_TIME(FLOOR(f.arr_time/100)::int, (f.arr_time::int%100), 0) AS arr_time_f,
+	   MAKE_TIME(FLOOR(f.arr_time/100)::int, (f.arr_time::int%100), 0) - MAKE_TIME(FLOOR(f.dep_time/100)::int, (f.dep_time::int%100), 0) AS delta_air_time,
+	   -- air_time,
+	   MAKE_INTERVAL(mins => (f.air_time::int)) AS air_time_f,
+	   -- dep_delay,
+	   MAKE_INTERVAL(mins => (f.dep_delay ::int)) AS dep_delay_f,
+	   -- arr_delay,
+	   MAKE_INTERVAL(mins => (f.arr_delay ::int)) AS arr_delay_f,
+	   airline, -- Difference between airlines
+	   tail_number,
+	   diverted,
+	   cancelled
+FROM flights_api_proj_gr4 f;
 
 
 -- experimental
@@ -75,4 +91,3 @@ FROM flights_api_proj_gr4 f
 LEFT JOIN airports a
 	ON f.origin = a.faa
 WHERE MAKE_TIME(FLOOR(arr_time/100)::int, (arr_time::int%100), 0) - MAKE_TIME(FLOOR(dep_time/100)::int, (dep_time::int%100), 0) != MAKE_INTERVAL(mins => (air_time::int));
-
