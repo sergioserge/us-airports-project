b1d53a6ab205c41ff4c3b8bbd20683e605fb0cc0
“sergioserge” <sergej.lust01@gmail.com>
2021-11-08 11:37:57 +0100

    update 1.2

--- sql.py
+++ sql.py
@@ -20,32 +20,17 @@ def get_conn():
 def get_engine():
     return create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}')
 
+# get data as dataframe from database
+def query_database(query_string):
+    engine = get_engine()
+    with engine.connect() as connection:
+        result = connection.execute(query_string)
+        result = result.fetchall()
+        df = pd.DataFrame(result)
+    
+    return df
 
-# function get_data for sql
-def get_data(query):
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # create a connection to the PostgreSQL server
-        conn = psycopg2.connect(host=host,
-                        port=port,
-                        database=database,
-                        user=user,
-                        password=password)
-		
-        display(pd.read_sql_query(query, conn))
-       
-	    # close the connection to the PostgreSQL database
-        conn.close()
-
-    # the code below makes the function more robust, you can ignore this part
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-
+# save dataframe to database
 def save_to_db(df, table_name):
     # function to upload the data to the database
     engine = get_engine()
@@ -63,3 +48,24 @@ def save_to_db(df, table_name):
         except (Exception, psycopg2.DatabaseError) as error:
             engine = None
             return print(error)
+
+
+# function get_data for sql
+def get_data(query):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # create a connection to the PostgreSQL server
+        conn = get_conn()
+
+        display(pd.read_sql_query(query, conn))
+       
+	    # close the connection to the PostgreSQL database
+        conn.close()
+
+    # the code below makes the function more robust, you can ignore this part
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
